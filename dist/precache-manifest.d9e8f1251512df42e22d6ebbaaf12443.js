44555e5a088093d300950553d2b13cd8f5e3e72f
dacharat <dacharat.p@ku.th>
2019-02-15 00:11:49 +0700

    :octocat: complete deployment

---
+++ dist/precache-manifest.d9e8f1251512df42e22d6ebbaaf12443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9bd6216844bebd9017e8",
+    "url": "/static/js/main.9bd62168.chunk.js"
+  },
+  {
+    "revision": "de3374a1619d9c47989e",
+    "url": "/static/js/2.de3374a1.chunk.js"
+  },
+  {
+    "revision": "9bd6216844bebd9017e8",
+    "url": "/static/css/main.1a3023b4.chunk.css"
+  },
+  {
+    "revision": "de3374a1619d9c47989e",
+    "url": "/static/css/2.a7df5893.chunk.css"
+  },
+  {
+    "revision": "7cf54d56c5f253a637682b5f0774412f",
+    "url": "/index.html"
+  }
+];
