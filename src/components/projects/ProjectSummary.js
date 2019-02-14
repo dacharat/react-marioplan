7f98d2b86d8596329cc19bbb9b093eceea4dade9
dacharat <dacharat.p@ku.th>
2019-02-14 23:05:20 +0700

    :octocat: init firebase function to project

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,15 @@
 import React from "react";
+import moment from 'moment'
 
-const ProjectSummary = ({project}) => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by Jack</p>
-        <p className="grey-text">28/3/98 </p>
+        <p>
+          Posted by {project.authorFirstName + " " + project.authorLastName}
+        </p>
+        <p className="grey-text">{moment(project.createAt.toDate()).calendar()}</p>
       </div>
     </div>
   );
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,11 +2,13 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make asyn call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile
+    const authID = getState().firebase.auth.uid
     firestore.collection("projects").add({
       ...project,
-      authorFirstName: "Dacharat",
-      authorLastName: "Pankong",
-      authorId: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authID,
       createAt: new Date()
     }).then(() => {
       dispatch({ type: "CREATE_PROJECT", project });
